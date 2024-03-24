00558366cad386c1cf630215c823772346e139cd
Macksn <itsmacksn@yahoo.com>
2024-03-24 22:16:11 +0300

    Arka_Plan_Renk_Degisimi
    Arka plan rengini değiştirme

---
+++ Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace colour
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "pembe",
+            "mavi",
+            "kırmızı",
+            "sarı",
+            "yeşil",
+            "mor",
+            "turuncu"});
+            this.listBox1.Location = new System.Drawing.Point(128, 81);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace colour
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (listBox1.Text)
+            {
+                case "pembe": 
+                    this.BackColor = Color.Pink;
+                    break;
+                case "mavi":
+                    this.BackColor = Color.Blue;
+                    break;
+                case "kırmızı":
+                    this.BackColor = Color.Red;
+                    break;
+                case "sarı":
+                    this.BackColor = Color.Yellow;
+                    break;
+                case "yeşil":
+                    this.BackColor = Color.Green;
+                    break;
+                case "mor":
+                    this.BackColor = Color.Purple;
+                    break;
+                case "turuncu":
+                    this.BackColor = Color.Orange;
+                    break;
+                   
+            }
+
+
+        }
+    }
+}
